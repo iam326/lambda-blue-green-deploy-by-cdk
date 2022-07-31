8481ef0e4982e273750b3ab5db581f57ed81bffa
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2022-07-31 15:26:02 +0900

    add lambda & apigw

--- lib/lambda-blue-green-deploy-by-cdk-stack.ts
+++ lib/lambda-blue-green-deploy-by-cdk-stack.ts
@@ -1,16 +1,37 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as path from 'path';
+
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_apigateway as apigateway } from 'aws-cdk-lib';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
+import { aws_lambda_nodejs as node_lambda } from 'aws-cdk-lib';
 
 export class LambdaBlueGreenDeployByCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaFunction = new node_lambda.NodejsFunction(
+      this,
+      'LambdaBlueGreenDeployByCdkFunction',
+      {
+        functionName: 'LambdaBlueGreenDeployByCdkFunction',
+        description: `Generated on: ${new Date().toISOString()}`,
+        runtime: lambda.Runtime.NODEJS_14_X,
+        entry: path.join(__dirname, '../src/lambda/index.ts'),
+        handler: 'handler',
+        currentVersionOptions: {
+          removalPolicy: RemovalPolicy.RETAIN,
+        },
+      }
+    );
+    const lambdaAlias = lambdaFunction.currentVersion.addAlias('alias');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaBlueGreenDeployByCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const restApi = new apigateway.RestApi(this, 'RestApi', {
+      restApiName: 'LambdaBlueGreenDeployByCdkFunctionApi',
+    });
+    restApi.root.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(lambdaAlias)
+    );
   }
 }
